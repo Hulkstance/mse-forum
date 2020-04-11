c97d0f80d4c7dd59bc7c09a930b0d1b584fd2c87
Chrome <warrolen94@gmail.com>
2020-04-11 18:39:31 +0300

    CSRF Disable

--- forum/src/main/java/com/mse/wcp/forum/security/AuthorizationServerConfig.java
+++ forum/src/main/java/com/mse/wcp/forum/security/AuthorizationServerConfig.java
@@ -4,6 +4,7 @@ import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.RSAKey;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -78,7 +79,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
-                .authenticationManager(this.authenticationManager)
+                .authenticationManager(authenticationManager)
                 .tokenStore(tokenStore())
                 .userApprovalHandler(userApprovalHandler())
                 .accessTokenConverter(accessTokenConverter());
--- forum/src/main/java/com/mse/wcp/forum/security/SecurityConfig.java
+++ forum/src/main/java/com/mse/wcp/forum/security/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/oauth2/keys").permitAll()
                 .anyRequest().authenticated();
