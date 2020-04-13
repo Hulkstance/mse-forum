490eb534dd226e5c513507a72265fe42da0b891a
Chrome <warrolen94@gmail.com>
2020-04-13 21:54:25 +0300

    Stackoverflow guy's change

--- forum/src/main/java/com/mse/wcp/forum/security/AuthorizationServerConfig.java
+++ forum/src/main/java/com/mse/wcp/forum/security/AuthorizationServerConfig.java
@@ -1,5 +1,6 @@
 package com.mse.wcp.forum.security;
 
+import com.mse.wcp.forum.service.impl.UserDetailsServiceImpl;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.KeyUse;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.jwt.crypto.sign.RsaSigner;
@@ -23,17 +25,19 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.approval.ApprovalStore;
-import org.springframework.security.oauth2.provider.approval.ApprovalStoreUserApprovalHandler;
-import org.springframework.security.oauth2.provider.approval.InMemoryApprovalStore;
-import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
+import org.springframework.security.oauth2.provider.approval.*;
 import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.time.Instant;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * The Authorization Server is responsible for generating tokens specific to a client.
@@ -62,6 +66,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     private AuthenticationManager authenticationManager;
 
     @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
@@ -72,15 +79,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .secret(bCryptPasswordEncoder.encode(clientSecret))
                 .authorizedGrantTypes("password", "authorization_code", "refresh_token")
                 .scopes("read", "write", "trust")
-                .resourceIds("api")
                 .accessTokenValiditySeconds(accessTokenValidity)
-                .refreshTokenValiditySeconds(refreshTokenValidity);
+                .refreshTokenValiditySeconds(refreshTokenValidity)
+                .resourceIds("api");
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
                 .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService)
                 .tokenStore(tokenStore())
                 .userApprovalHandler(userApprovalHandler())
                 .accessTokenConverter(accessTokenConverter());
@@ -129,7 +137,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Bean
     public ApprovalStore approvalStore() {
-        return new InMemoryApprovalStore();
+        InMemoryApprovalStore approvalStore = new InMemoryApprovalStore();
+        Date expirationDate = Date.from(Instant.now().plusSeconds(3600));
+        List<Approval> approvals = Stream.of("read", "write", "trust")
+                .map(scope -> new Approval("admin", "trusted", scope, expirationDate,
+                        Approval.ApprovalStatus.APPROVED))
+                .collect(Collectors.toList());
+        approvalStore.addApprovals(approvals);
+        return approvalStore;
     }
 
     @Bean
